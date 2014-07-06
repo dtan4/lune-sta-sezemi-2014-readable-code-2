76c65f4ba155ebbb6ffb738052e03c47674e8f4f
lune* <lune@wuvu.net>
2014-07-06 11:06:00 +0900

    spec5: レシピにIDをわりふるように修正

--- recipe.rb
+++ recipe.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/ruby
 
 file_name = ARGV[0]
-recipe_data = File.open(file_name).read
-puts recipe_data
+recipe_data_array = File.open(file_name).read.split("\n")
 
+id_num = 0
+recipe_data_array.each do |recipe|
+  id_num += 1
+  puts "#{id_num}: #{recipe}"
+end
