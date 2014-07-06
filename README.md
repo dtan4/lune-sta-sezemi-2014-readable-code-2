dfd0efbb365361c9cc7bdf2a727e10babdb87593
lune* <lune@wuvu.net>
2014-07-06 11:08:40 +0900

    spec6: レシピの出力にIDを指定できるように修正

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ lune-sta-sezemi-2014-readable-code-2
 
 実行方法
 ---------------------------------
-% ./recipe.rb recipe-data.txt
+* % ./recipe.rb recipe-data.txt id
--- recipe.rb
+++ recipe.rb
@@ -1,10 +1,38 @@
 #!/usr/bin/ruby
 
 file_name = ARGV[0]
-recipe_data_array = File.open(file_name).read.split("\n")
+id_num_opt = ARGV[1]
 
-id_num = 0
-recipe_data_array.each do |recipe|
-  id_num += 1
-  puts "#{id_num}: #{recipe}"
+class RecipeDatabase
+
+  def initialize
+    @recipes = Hash.new()
+    @last_id_num = 0
+  end
+
+  def add_recipe_file(file_name)
+    File.open(file_name).read.split("\n").each do |line|
+      @last_id_num += 1
+      @recipes[@last_id_num] = {
+        'name' => line
+      }
+    end
+  end
+
+  def puts_recipe(id_num)
+    name = @recipes[id_num]['name']
+    puts "#{id_num}: #{name}"
+  end
+
+  def puts_all_recipe
+    @recipes.keys.each do |id_num|
+      puts_recipe(id_num)
+    end
+  end
+
 end
+
+recipes = RecipeDatabase.new
+recipes.add_recipe_file(file_name)
+id_num_opt.nil? ? recipes.puts_all_recipe : recipes.puts_recipe(id_num_opt.to_i)
+
