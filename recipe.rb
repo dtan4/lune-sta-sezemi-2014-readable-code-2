a574b8ca5794422a459f27e828e7c7984b5af500
dtan4 <dtanshi45@gmail.com>
2014-07-06 14:30:30 +0900

    spec9: Show recipes by user

--- recipe.rb
+++ recipe.rb
@@ -2,18 +2,19 @@
 
 user = ARGV[0]
 file_name = ARGV[1]
-id_num_opt = ARGV.size.odd? ? nil : ARGV[ARGV.size -1].to_i
+# id_num_opt = ARGV.size.odd? ? nil : ARGV[ARGV.size -1].to_i
 
 class RecipeData
+  attr_reader :users
 
   def initialize
     @users = {}
     @recipes = {}
-    
-    @last_user_id_num = 
+
+    @last_user_id_num =
     @last_recipe_id_num = 0
   end
-  
+
   def add_user(user_name)
     @last_user_id_num += 1
     @users[@last_user_id_num] = {
@@ -32,9 +33,9 @@ class RecipeData
       }
     end
   end
-  
+
   def to_a(id_num = nil)
-    
+
   end
 
   def get_recipe(id_num)
@@ -52,6 +53,17 @@ class RecipeData
     return result.flatten
   end
 
+  def recipes_by_user(user_id)
+    @recipes.values.select { |recipe| recipe['user_id'] == user_id }
+  end
+
+  def user_id_of(user_name)
+    @users.each do |id, user|
+      return id if user['name'] == user_name
+    end
+
+    -1
+  end
 end
 
 recipes = RecipeData.new
@@ -64,5 +76,10 @@ recipes = RecipeData.new
   recipes.add_recipe_file(file_name)
 end
 
-puts recipes.to_a(id_num_opt)
-
+recipes.users.each do |user_id, user|
+  puts "ユーザー名: #{user['name']}"
+  recipes.recipes_by_user(user_id).each do |recipe|
+    puts "#{recipe['name']} #{recipe['url']}"
+  end
+  puts ""
+end
